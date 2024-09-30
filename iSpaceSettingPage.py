e9c18e7d0df907bbf6c8220f362f40845e637bca
YoonFoo <yoonfoo1102@gmail.com>
2024-10-01 00:18:43 +0800

    4F discussion room added

--- iSpaceSettingPage.py
+++ iSpaceSettingPage.py
@@ -35,27 +35,33 @@ class ISpaceSettingPage(QtWidgets.QWidget):
             margin-left: 5px;
         '''
 
+    ### #################################################################################
+
         layout_widget = QtWidgets.QWidget(self)
         layout_widget.setStyleSheet(layout_style)
         layout_widget.setGeometry(80,180,1300,500)
         # layout_widget.setGeometry(150,250,1500,500)
         self.layout = QtWidgets.QGridLayout(layout_widget)
 
+    ### #################################################################################
+
         start_time = datetime.strptime('00:00', '%H:%M')
         end_time = datetime.strptime('23:59', '%H:%M')
         time_intervals = self.generate_time_intervals(start_time, end_time, 30)
-
-        self.time_list = []
         
+    ### #################################################################################
+
         self.container_floor = QtWidgets.QWidget(self)
         self.floor = QtWidgets.QComboBox(self.container_floor)
         self.floor.setStyleSheet(select_style)
         self.floor.setFixedSize(150,30)
-        self.floor.addItems(['','圖書館1F', '圖書館6F', '圖書館7F', '圖書館8F'])
+        self.floor.addItems(['','圖書館1F', '圖書館4F', '圖書館6F', '圖書館7F', '圖書館8F'])
         self.floor.currentIndexChanged.connect(self.rooms_selections)
         self.layout.addWidget(self.floor, 0, 0)
         self.layout.setAlignment(self.floor, QtCore.Qt.AlignmentFlag.AlignCenter)
         self.layout.setColumnMinimumWidth(0, 400)
+
+    ### #################################################################################
 
         self.container = QtWidgets.QWidget(self)
         self.suspend_all = QtWidgets.QCheckBox(self.container)
@@ -67,24 +73,31 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         self.layout.addWidget(self.suspend_all, 1, 0)
         self.layout.setAlignment(self.suspend_all, QtCore.Qt.AlignmentFlag.AlignCenter)
 
+    ### #################################################################################
+
         self.room_layout = QtWidgets.QHBoxLayout()
         self.room_1 = QtWidgets.QCheckBox(self)
         self.room_2 = QtWidgets.QCheckBox(self)
+        self.room_3 = QtWidgets.QCheckBox(self)
 
         self.room_1.setStyleSheet(label_style)
         self.room_2.setStyleSheet(label_style)
+        self.room_3.setStyleSheet(label_style)
 
         self.room_layout.addWidget(self.room_1)
         self.room_layout.addWidget(self.room_2)
-        self.room_layout.setAlignment(self.room_1, QtCore.Qt.AlignmentFlag.AlignRight)
-        self.room_layout.setAlignment(self.room_2, QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.room_layout.addWidget(self.room_3)
+
         self.layout.addLayout(self.room_layout, 2, 0)
         self.room_1.setVisible(False)
         self.room_2.setVisible(False)
+        self.room_3.setVisible(False)
 
+    ### #################################################################################
 
         column_index = 1
         row_index = 0
+        self.time_list = []
         
         for i in range(48):
             
@@ -100,6 +113,8 @@ class ISpaceSettingPage(QtWidgets.QWidget):
             if column_index == 9: 
                 column_index = 1
                 row_index += 1
+    
+    ### #################################################################################
 
         self.suspend_date_container_a = QtWidgets.QHBoxLayout()
         self.suspend_date_start = QtWidgets.QDateEdit(calendarPopup=True)
@@ -127,6 +142,8 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         """)
         self.suspend_date_start.setMinimumDate(QtCore.QDate.currentDate())
         self.suspend_date_start.setCalendarWidget(self.suspend_date_start_calendar)
+
+    ### #################################################################################
 
         self.suspend_date_container_b = QtWidgets.QHBoxLayout()
         self.suspend_date_end = QtWidgets.QDateEdit(calendarPopup=True)
@@ -158,6 +175,8 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         self.layout.addLayout(self.suspend_date_container_a, 3, 0)
         self.layout.addLayout(self.suspend_date_container_b, 4, 0)
       
+    ### #################################################################################
+
         self.suspend_reason_container = QtWidgets.QHBoxLayout()
         self.suspend_reason_label = QtWidgets.QLabel(self)
         self.suspend_reason_label.setText('暫停使用原因: ')
@@ -169,6 +188,8 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         self.suspend_reason_container.addWidget(self.suspend_reason)
 
         self.layout.addLayout(self.suspend_reason_container, 5, 0)
+
+    ### #################################################################################
 
         self.add_suspend_button = QtWidgets.QPushButton(self)
         self.add_suspend_button.setText('新增暫停使用空間')
@@ -187,8 +208,7 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         self.add_suspend_button.clicked.connect(self.suspend_submit)
         self.layout.addWidget(self.add_suspend_button, 8, 7, 1, 2)
 
-        self.suspend_submit()
-
+    ### #################################################################################
 
     def generate_time_intervals(self, start_time, end_time, interval_minutes):
         intervals = []
@@ -199,24 +219,44 @@ class ISpaceSettingPage(QtWidgets.QWidget):
             current_time += timedelta(minutes=interval_minutes)
         return intervals
 
+    ### #################################################################################
+
     def rooms_selections(self):
+
+        self.room_layout.setAlignment(self.room_1, QtCore.Qt.AlignmentFlag.AlignRight)
+        self.room_layout.setAlignment(self.room_2, QtCore.Qt.AlignmentFlag.AlignLeft)
 
         current_floors = self.floor.currentText()
         if current_floors == '':
             self.room_1.setVisible(False)
             self.room_2.setVisible(False)
+            self.room_3.setVisible(False)
             self.suspend_all.setDisabled(True)
 
         elif current_floors == '圖書館1F':
             self.room_1.setVisible(True)
             self.room_2.setVisible(True)
+            self.room_3.setVisible(False)
             self.room_1.setText('1A')
             self.room_2.setText('1B')
             self.suspend_all.setDisabled(False)
 
+        elif current_floors == '圖書館4F':
+            self.room_1.setVisible(True)
+            self.room_2.setVisible(True)
+            self.room_3.setVisible(True)
+            self.room_1.setText('4A')
+            self.room_2.setText('4B')
+            self.room_3.setText('4C')
+            self.suspend_all.setDisabled(False)
+            self.room_layout.setAlignment(self.room_1, QtCore.Qt.AlignmentFlag.AlignRight)
+            self.room_layout.setAlignment(self.room_2, QtCore.Qt.AlignmentFlag.AlignCenter)
+            self.room_layout.setAlignment(self.room_3, QtCore.Qt.AlignmentFlag.AlignLeft)
+
         elif current_floors == '圖書館6F':
             self.room_1.setVisible(True)
             self.room_2.setVisible(True)
+            self.room_3.setVisible(False)
             self.room_1.setText('6A')
             self.room_2.setText('6B')
             self.suspend_all.setDisabled(False)
@@ -224,6 +264,7 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         elif current_floors == '圖書館7F':
             self.room_1.setVisible(True)
             self.room_2.setVisible(True)
+            self.room_3.setVisible(False)
             self.room_1.setText('7A')
             self.room_2.setText('7B')
             self.suspend_all.setDisabled(False)
@@ -231,9 +272,12 @@ class ISpaceSettingPage(QtWidgets.QWidget):
         elif current_floors == '圖書館8F':
             self.room_1.setVisible(True)
             self.room_2.setVisible(True)
+            self.room_3.setVisible(False)
             self.room_1.setText('8A')
             self.room_2.setText('8B')
             self.suspend_all.setDisabled(False)
+
+    ### #################################################################################
 
     def checked_date(self, time):
         if time.isChecked():
@@ -243,11 +287,15 @@ class ISpaceSettingPage(QtWidgets.QWidget):
             if time.text() in self.checkedDate_list:
                 self.checkedDate_list.remove(time.text())
 
+    ### #################################################################################
 
     def suspend_all_rooms(self):
 
         self.room_1.setChecked(self.suspend_all.isChecked())
         self.room_2.setChecked(self.suspend_all.isChecked())
+        self.room_3.setChecked(self.suspend_all.isChecked())
+
+    ### #################################################################################
 
     def suspend_submit(self):
         start_date = self.suspend_date_start.date()
@@ -259,7 +307,8 @@ class ISpaceSettingPage(QtWidgets.QWidget):
                 self.floor.currentText(), 
                 self.suspend_all.isChecked(), 
                 self.room_1.isChecked(), 
-                self.room_2.isChecked(), 
+                self.room_2.isChecked(),
+                self.room_3.isChecked(), 
                 self.checkedDate_list,
                 self.suspend_reason.text()
                 )
@@ -272,6 +321,7 @@ class ISpaceSettingPage(QtWidgets.QWidget):
                     self.suspend_all.isChecked(), 
                     self.room_1.isChecked(), 
                     self.room_2.isChecked(), 
+                    self.room_3.isChecked(), 
                     self.checkedDate_list,
                     self.suspend_reason.text()
                     )
